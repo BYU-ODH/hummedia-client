5e70868b785831cb13630abba1c868a3f9638a0e
alexcdahl <alex.c.dahl@gmail.com>
2017-04-25 14:39:17 -0600

    Updated styling.

--- app/js/controllers/help.js
+++ app/js/controllers/help.js
@@ -1,6 +1,6 @@
 function HelpCtrl($scope, $routeParams, $location) {
     $scope.sections = ['about', 'annotationTypes', 'addingAnnotations', 'addingContent', 'addingSubtitles', 'creatingCollections', 'linkingYoutube', 'usingCliplists', 'viewingSubtitles', 'cannotSeeCourse', 'extra'];
-    $scope.titles = ['About', 'Annotation Types', 'Adding Annotations', 'Adding Conent to Your Collection', 'Adding Subtitles', 'Creating a Collection', 'Linking Content Via YouTube', 'Using Cliplists', 'Viewing Subtitles', "Students can't see my course!", 'Extra Help']
+    $scope.titles = ['About', 'Annotation Types', 'Adding Annotations', 'Adding Content to Your Collection', 'Adding Subtitles', 'Creating a Collection', 'Linking Content Via YouTube', 'Using Cliplists', 'Viewing Subtitles', "Students can't see my course!", 'Extra Help']
     $scope.helpSection = "../../help/about.txt";
     $scope.selectedSection = 'about';
     $scope.changeView = function(section) {
